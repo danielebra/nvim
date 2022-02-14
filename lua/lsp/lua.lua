d65ef6f127b20d45d01cb3437ce36dc5b7e66ae2
Daniel Ebrahimian <git@danielebra.com>
2022-02-14 21:22:15 +1100

    Identify what slows down UI renders

--- lua/lsp/lua.lua
+++ lua/lsp/lua.lua
@@ -8,7 +8,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     virtual_text = {
       prefix = "",
-      spacing = 0,
+      spacing = 1,
     },
     signs = true,
     underline = true,
