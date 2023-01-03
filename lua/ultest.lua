377e8a2b8a16b5cd927c585de56574b7a4a074ba
Daniel Ebrahimian <git@danielebra.com>
2023-01-03 17:04:52 +1100

    Add icons

--- lua/ultest.lua
+++ lua/ultest.lua
@@ -1,6 +1,21 @@
 -- vim.g.ultest_running_sign = "羽"
--- TODO: Icons, notifications
+-- TODO: notifications
 require("neotest").setup({
+  icons = {
+      child_indent = "│",
+      child_prefix = "├",
+      collapsed = "─",
+      expanded = "╮",
+      failed = "✖",
+      final_child_indent = " ",
+      final_child_prefix = "╰",
+      non_collapsible = "─",
+      passed = "✔",
+      running = "羽",
+      running_animated = { "/", "|", "\\", "-", "/", "|", "\\", "-" },
+      skipped = "-",
+      unknown = "?"
+    },
   adapters = {
     require("neotest-python")({
       dap = { justMyCode = false },
