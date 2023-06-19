bf495abe64ade5947e684fed7eb69e126bf6202b
Daniel Ebrahimian <git@danielebra.com>
2023-06-19 11:10:51 +1000

    Adjust for 0.9

--- lua/file_tree.lua
+++ lua/file_tree.lua
@@ -4,10 +4,6 @@ require'nvim-tree'.setup {-- following options are the default
   disable_netrw       = false,
   -- hijack netrw window on startup
   hijack_netrw        = true,
-  -- open the tree when running this setup function
-  open_on_setup       = false,
-  -- will not open on setup if the filetype is in this list
-  ignore_ft_on_setup  = {},
   -- closes neovim automatically when the tree is the last **WINDOW** in the view
   -- auto_close          = true,
   -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
