5542c87b713bef9cb571c9f6eb72746e2068da4c
Daniel Ebrahimian <git@danielebra.com>
2024-07-03 15:26:24 +1000

    Remove gitsign highlight grouping

--- lua/plugin_configs/gitsigns.lua
+++ lua/plugin_configs/gitsigns.lua
@@ -30,15 +30,3 @@ require('gitsigns').setup {
     update_debounce = 200,
     status_formatter = nil, -- Use default
   }
-
-vim.api.nvim_set_hl(0, 'GitSignsAdd', { link = 'GitSignsAdd'})
-vim.api.nvim_set_hl(0, 'GitSignsAddLn', { link = 'GitSignsAddLn'})
-vim.api.nvim_set_hl(0, 'GitSignsAddNr', { link = 'GitSignsAddNr'})
-
-vim.api.nvim_set_hl(0, 'GitSignsChange', { link = 'GitSignsChange'})
-vim.api.nvim_set_hl(0, 'GitSignsChangeNr', { link = 'GitSignsChangeNr'})
-vim.api.nvim_set_hl(0, 'GitSignsChangeLn', { link = 'GitSignsChangeLn'})
-
-vim.api.nvim_set_hl(0, 'GitSignsDelete', { link = 'GitSignsDelete'})
-vim.api.nvim_set_hl(0, 'GitSignsDeleteNr', { link = 'GitSignsDeleteNr'})
-vim.api.nvim_set_hl(0, 'GitSignsDeleteLn', { link = 'GitSignsDeleteLn'})
